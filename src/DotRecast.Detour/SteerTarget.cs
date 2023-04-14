af6c8043300679fb6dbdae879f87638a589ede4e
ikpil <ikpil@naver.com>
2023-04-14 22:29:19 +0900

    move utils

---
+++ src/DotRecast.Detour/SteerTarget.cs
@@ -0,0 +1,20 @@
+﻿using DotRecast.Core;
+
+namespace DotRecast.Detour
+{
+    public class SteerTarget
+    {
+        public readonly Vector3f steerPos;
+        public readonly int steerPosFlag;
+        public readonly long steerPosRef;
+        public readonly float[] steerPoints;
+
+        public SteerTarget(Vector3f steerPos, int steerPosFlag, long steerPosRef, float[] steerPoints)
+        {
+            this.steerPos = steerPos;
+            this.steerPosFlag = steerPosFlag;
+            this.steerPosRef = steerPosRef;
+            this.steerPoints = steerPoints;
+        }
+    }
+}
