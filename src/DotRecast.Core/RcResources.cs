c294275da4b1cdab172c598f94ed572e0354d5df
ikpil <ikpil0@gmail.com>
2023-09-23 14:47:04 +0900

    bugfix: not working reousrce.load

--- src/DotRecast.Core/RcResources.cs
+++ src/DotRecast.Core/RcResources.cs
@@ -6,7 +6,12 @@ namespace DotRecast.Core
     {
         public static byte[] Load(string filename)
         {
-            var filepath = RcDirectory.SearchFile($"resources/{filename}");
+            var filepath = filename;
+            if (!File.Exists(filepath))
+            {
+                filepath = RcDirectory.SearchFile($"resources/{filename}");
+            }
+            
             using var fs = new FileStream(filepath, FileMode.Open, FileAccess.Read, FileShare.Read);
             byte[] buffer = new byte[fs.Length];
             fs.Read(buffer, 0, buffer.Length);
