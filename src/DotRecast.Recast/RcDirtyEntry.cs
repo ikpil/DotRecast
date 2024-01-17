78b29920cbe331c23f2ccf327d5373c436710cb3
ikpil <ikpil@naver.com>
2024-01-17 13:54:46 +0900

    add RcDirtyEntry, RcLevelStackEntry

---
+++ src/DotRecast.Recast/RcDirtyEntry.cs
@@ -0,0 +1,16 @@
+﻿namespace DotRecast.Recast
+{
+    public readonly struct RcDirtyEntry
+    {
+        public readonly int index;
+        public readonly int region;
+        public readonly int distance2;
+
+        public RcDirtyEntry(int tempIndex, int tempRegion, int tempDistance2)
+        {
+            index = tempIndex;
+            region = tempRegion;
+            distance2 = tempDistance2;
+        }
+    }
+}
---
+++ src/DotRecast.Recast/RcLevelStackEntry.cs
@@ -0,0 +1,16 @@
+﻿namespace DotRecast.Recast
+{
+    public readonly struct RcLevelStackEntry
+    {
+        public readonly int x;
+        public readonly int y;
+        public readonly int index;
+
+        public RcLevelStackEntry(int tempX, int tempY, int tempIndex)
+        {
+            x = tempX;
+            y = tempY;
+            index = tempIndex;
+        }
+    }
+}
