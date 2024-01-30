aaff85b2295a5e268723761bc59383bcc470f15f
ikpil <ikpil@naver.com>
2024-01-31 00:59:48 +0900

    fix: Change DtRaycastHit class to struct to resolve SOH issue. step2

--- src/DotRecast.Detour/DtRaycastHit.cs
+++ src/DotRecast.Detour/DtRaycastHit.cs
@@ -26,22 +26,30 @@ namespace DotRecast.Detour
     /// Provides information about raycast hit
     /// filled by dtNavMeshQuery::raycast
     /// @ingroup detour
-    public class DtRaycastHit
+    public struct DtRaycastHit
     {
         /// The hit parameter. (FLT_MAX if no wall hit.)
         public float t;
 
         /// hitNormal	The normal of the nearest wall hit. [(x, y, z)]
-        public RcVec3f hitNormal = new RcVec3f();
+        public RcVec3f hitNormal;
 
         /// The index of the edge on the final polygon where the wall was hit.
         public int hitEdgeIndex;
-        
+
         /// Pointer to an array of reference ids of the visited polygons. [opt]
-        public readonly List<long> path = new List<long>();
+        public readonly List<long> path;
 
         ///  The cost of the path until hit.
         public float pathCost;
 
+        public DtRaycastHit(int s)
+        {
+            t = 0;
+            hitEdgeIndex = 0;
+            hitNormal = RcVec3f.Zero;
+            path = new List<long>();
+            pathCost = 0;
+        }
     }
 }
