5bba84883e510e7d146523785eaffa2fd9284739
ikpil <ikpil@naver.com>
2024-01-31 00:13:23 +0900

    clean up comments in DtRaycastHit

--- src/DotRecast.Detour/DtRaycastHit.cs
+++ src/DotRecast.Detour/DtRaycastHit.cs
@@ -23,24 +23,25 @@ using DotRecast.Core.Numerics;
 
 namespace DotRecast.Detour
 {
-    /**
- * Provides information about raycast hit. Filled by NavMeshQuery::raycast
- */
+    /// Provides information about raycast hit
+    /// filled by dtNavMeshQuery::raycast
+    /// @ingroup detour
     public class DtRaycastHit
     {
-        /** The hit parameter. (float.MaxValue if no wall hit.) */
+        /// The hit parameter. (FLT_MAX if no wall hit.)
         public float t;
 
-        /** hitNormal The normal of the nearest wall hit. [(x, y, z)] */
+        /// hitNormal	The normal of the nearest wall hit. [(x, y, z)]
         public RcVec3f hitNormal = new RcVec3f();
 
-        /** Visited polygons. */
+        /// The index of the edge on the final polygon where the wall was hit.
+        public int hitEdgeIndex;
+        
+        /// Pointer to an array of reference ids of the visited polygons. [opt]
         public readonly List<long> path = new List<long>();
 
-        /** The cost of the path until hit. */
+        ///  The cost of the path until hit.
         public float pathCost;
 
-        /** The index of the edge on the readonly polygon where the wall was hit. */
-        public int hitEdgeIndex;
     }
 }
