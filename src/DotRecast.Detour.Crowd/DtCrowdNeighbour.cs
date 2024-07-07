76e5ade4d128d17b6129c664f6d32a648c406e5c
ikpil <ikpil0@gmail.com>
2024-07-07 14:04:37 +0900

    update comment in DtCrowdNeighbour

--- src/DotRecast.Detour.Crowd/DtCrowdNeighbour.cs
+++ src/DotRecast.Detour.Crowd/DtCrowdNeighbour.cs
@@ -5,12 +5,9 @@
     /// @see dtCrowdAgent::neis, dtCrowd
     public readonly struct DtCrowdNeighbour
     {
-        public readonly DtCrowdAgent agent;
-
-        /// < The index of the neighbor in the crowd.
-        public readonly float dist;
-
-        /// < The distance between the current agent and the neighbor.
+        public readonly DtCrowdAgent agent; // < The index of the neighbor in the crowd.
+        public readonly float dist; // < The distance between the current agent and the neighbor.
+        
         public DtCrowdNeighbour(DtCrowdAgent agent, float dist)
         {
             this.agent = agent;
