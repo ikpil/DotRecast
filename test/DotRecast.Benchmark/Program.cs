b032b5900b4bbd0cbc920fd2fa471a8fe1a7349f
Sarofc <sarono@foxmail.com>
2024-07-07 19:05:35 +0800

    opt UpdateMoveRequest UpdateTopologyOptimization

--- test/DotRecast.Benchmark/Program.cs
+++ test/DotRecast.Benchmark/Program.cs
@@ -2,7 +2,6 @@ using BenchmarkDotNet.Running;
 using CSharpBencchmark;
 
 BenchmarkRunner.Run([
-    //BenchmarkConverter.TypeToBenchmarks(typeof(Bench_PriorityQueue)),
+    BenchmarkConverter.TypeToBenchmarks(typeof(Bench_PriorityQueue)),
     //BenchmarkConverter.TypeToBenchmarks(typeof(Bench_Math_RcVec3f)),
-    BenchmarkConverter.TypeToBenchmarks(typeof(Bench_stackalloc)),
 ]);
