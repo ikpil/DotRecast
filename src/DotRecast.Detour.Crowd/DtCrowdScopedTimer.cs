6eb38ec00e4e748582ebdecaa7b7f5d5edf57ccd
Sarofc <sarono@foxmail.com>
2024-07-13 19:15:26 +0800

    profile

--- src/DotRecast.Detour.Crowd/DtCrowdScopedTimer.cs
+++ src/DotRecast.Detour.Crowd/DtCrowdScopedTimer.cs
@@ -1,23 +1,30 @@
-﻿using System;
+
+using System;
 
 namespace DotRecast.Detour.Crowd
 {
-    internal readonly struct DtCrowdScopedTimer : IDisposable
+    internal readonly ref struct DtCrowdScopedTimer
     {
+#if PROFILE
         private readonly DtCrowdTimerLabel _label;
         private readonly DtCrowdTelemetry _telemetry;
+#endif
 
         internal DtCrowdScopedTimer(DtCrowdTelemetry telemetry, DtCrowdTimerLabel label)
         {
+#if PROFILE
             _telemetry = telemetry;
             _label = label;
 
             _telemetry.Start(_label);
+#endif
         }
 
         public void Dispose()
         {
+#if PROFILE
             _telemetry.Stop(_label);
+#endif
         }
     }
 }
