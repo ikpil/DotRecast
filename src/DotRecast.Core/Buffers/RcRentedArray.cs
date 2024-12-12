ea71ed080b7c5f2519e8340f9cd0ada6e29f0e74
ikpil <ikpil@naver.com>
2024-12-12 23:59:35 +0900

    Update RentIdPool visibility to internal and mark as sealed.

--- src/DotRecast.Core/Buffers/RcRentedArray.cs
+++ src/DotRecast.Core/Buffers/RcRentedArray.cs
@@ -6,46 +6,60 @@ using System.Threading;
 
 namespace DotRecast.Core.Buffers
 {
-    public static class RcRentedArray
+    public readonly struct RcRentIdGen
     {
-        private sealed class RentIdPool
+        public readonly int Id;
+        public readonly int Gen;
+
+        public RcRentIdGen(int id, int gen)
         {
-            private int[] _generations;
-            private readonly Queue<int> _freeIds;
-            private int _maxId;
+            Id = id;
+            Gen = gen;
+        }
+    }
+    
+    internal sealed class RcRentIdPool
+    {
+        private int[] _generations;
+        private readonly Queue<int> _freeIds;
+        private int _maxId;
 
-            public RentIdPool(int capacity)
-            {
-                _generations = new int[capacity];
-                _freeIds = new Queue<int>(capacity);
-            }
-
-            internal RentIdGen AcquireId()
-            {
-                if (!_freeIds.TryDequeue(out int id))
-                {
-                    id = _maxId++;
-                    if (_generations.Length <= id)
-                        Array.Resize(ref _generations, _generations.Length << 1);
-                }
-
-                return new RentIdGen(id, _generations[id]);
-            }
-
-            internal void ReturnId(int id)
-            {
-                _generations[id]++;
-                _freeIds.Enqueue(id);
-            }
-
-            internal int GetGeneration(int id)
-            {
-                return _generations.Length <= id ? 0 : _generations[id];
-            }
+        public RcRentIdPool(int capacity)
+        {
+            _generations = new int[capacity];
+            _freeIds = new Queue<int>(capacity);
         }
 
+        internal RcRentIdGen AcquireId()
+        {
+            if (!_freeIds.TryDequeue(out int id))
+            {
+                id = _maxId++;
+                if (_generations.Length <= id)
+                {
+                    Array.Resize(ref _generations, _generations.Length << 1);
+                }
+            }
+
+            return new RcRentIdGen(id, _generations[id]);
+        }
+
+        internal void ReturnId(int id)
+        {
+            _generations[id]++;
+            _freeIds.Enqueue(id);
+        }
+
+        internal int GetGeneration(int id)
+        {
+            return _generations.Length <= id ? 0 : _generations[id];
+        }
+    }
+
+    public static class RcRentedArray
+    {
         public const int START_RENT_ID_POOL_CAPACITY = 16;
-        private static readonly ThreadLocal<RentIdPool> _rentPool = new ThreadLocal<RentIdPool>(() => new RentIdPool(START_RENT_ID_POOL_CAPACITY));
+        private static readonly ThreadLocal<RcRentIdPool> _rentPool = new ThreadLocal<RcRentIdPool>(() => new RcRentIdPool(START_RENT_ID_POOL_CAPACITY));
 
         public static RcRentedArray<T> Rent<T>(int minimumLength)
         {
@@ -53,39 +67,29 @@ namespace DotRecast.Core.Buffers
             return new RcRentedArray<T>(ArrayPool<T>.Shared, _rentPool.Value.AcquireId(), array, minimumLength);
         }
 
-        internal static bool IsDisposed(RentIdGen rentIdGen)
+        internal static bool IsDisposed(RcRentIdGen rentIdGen)
         {
             return _rentPool.Value.GetGeneration(rentIdGen.Id) != rentIdGen.Gen;
         }
 
-        internal static void ReturnId(RentIdGen rentIdGen)
+        internal static void ReturnId(RcRentIdGen rentIdGen)
         {
             _rentPool.Value.ReturnId(rentIdGen.Id);
         }
     }
 
-    public readonly struct RentIdGen
-    {
-        public readonly int Id;
-        public readonly int Gen;
 
-        public RentIdGen(int id, int gen)
-        {
-            Id = id;
-            Gen = gen;
-        }
-    }
 
     public struct RcRentedArray<T> : IDisposable
     {
         private ArrayPool<T> _owner;
         private T[] _array;
-        private readonly RentIdGen _rentIdGen;
+        private readonly RcRentIdGen _rentIdGen;
 
         public int Length { get; }
         public bool IsDisposed => null == _owner || null == _array || RcRentedArray.IsDisposed(_rentIdGen);
 
-        internal RcRentedArray(ArrayPool<T> owner, RentIdGen rentIdGen, T[] array, int length)
+        internal RcRentedArray(ArrayPool<T> owner, RcRentIdGen rentIdGen, T[] array, int length)
         {
             _owner = owner;
             _array = array;
