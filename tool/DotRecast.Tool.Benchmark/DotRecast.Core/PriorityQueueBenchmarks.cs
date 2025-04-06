1fcbf5043b2e817df80639931a1adbd167b81be9
ikpil <ikpil@naver.com>
2025-04-06 13:30:12 +0900

    changed PriorityQueueBenchmarks

--- tool/DotRecast.Tool.Benchmark/DotRecast.Core/PriorityQueueBenchmarks.cs
+++ tool/DotRecast.Tool.Benchmark/DotRecast.Core/PriorityQueueBenchmarks.cs
@@ -6,88 +6,61 @@ using DotRecast.Core.Collections;
 namespace DotRecast.Tool.Benchmark.DotRecast.Core;
 
 /*
+BenchmarkDotNet v0.14.0, Windows 11 (10.0.26100.3624)
+AMD Ryzen 5 5625U, 1 CPU, 12 logical and 6 physical cores
+.NET SDK 9.0.200
+  [Host]     : .NET 9.0.2 (9.0.225.6610), X64 RyuJIT AVX2
+  DefaultJob : .NET 9.0.2 (9.0.225.6610), X64 RyuJIT AVX2
 
-// * Summary *
-BenchmarkDotNet v0.13.12, Windows 11 (10.0.22631.3958/23H2/2023Update/SunValley3)
-AMD Ryzen 5 3600, 1 CPU, 12 logical and 6 physical cores
-.NET SDK 8.0.101
-  [Host]     : .NET 8.0.1 (8.0.123.58001), X64 RyuJIT AVX2
-  DefaultJob : .NET 8.0.1 (8.0.123.58001), X64 RyuJIT AVX2
 
-| Method                         | Count | Mean                | Error            | StdDev           |
-|------------------------------- |------ |--------------------:|-----------------:|-----------------:|
-| Enqueue_RcSortedQueue          | 10    |            87.49 ns |         0.774 ns |         0.724 ns |
-| Enqueue_RcBinaryMinHeap        | 10    |           185.23 ns |         1.730 ns |         1.533 ns |
-| Enqueue_PriorityQueue          | 10    |           202.95 ns |         1.611 ns |         1.428 ns |
-| DequeueAll_RcSortedQueue       | 10    |           460.97 ns |         2.169 ns |         2.029 ns |
-| DequeueAll_RcBinaryMinHeap     | 10    |           573.17 ns |         2.542 ns |         2.378 ns |
-| DequeueAll_PriorityQueue       | 10    |           500.68 ns |         2.364 ns |         2.212 ns |
-| EnqueueDequeue_RcSortedQueue   | 10    |           525.43 ns |         1.842 ns |         1.632 ns |
-| EnqueueDequeue_RcBinaryMinHeap | 10    |           455.65 ns |         2.410 ns |         2.254 ns |
-| EnqueueDequeue_PriorityQueue   | 10    |           381.82 ns |         6.036 ns |         5.646 ns |
-| Enqueue_RcSortedQueue          | 100   |           730.57 ns |         5.229 ns |         4.635 ns |
-| Enqueue_RcBinaryMinHeap        | 100   |         3,012.15 ns |        10.875 ns |         9.640 ns |
-| Enqueue_PriorityQueue          | 100   |         2,306.80 ns |        26.694 ns |        23.663 ns |
-| DequeueAll_RcSortedQueue       | 100   |         6,241.67 ns |        31.856 ns |        29.798 ns |
-| DequeueAll_RcBinaryMinHeap     | 100   |        13,692.29 ns |        38.829 ns |        34.421 ns |
-| DequeueAll_PriorityQueue       | 100   |        12,482.93 ns |        93.955 ns |        87.886 ns |
-| EnqueueDequeue_RcSortedQueue   | 100   |        64,002.79 ns |       316.081 ns |       280.197 ns |
-| EnqueueDequeue_RcBinaryMinHeap | 100   |         8,655.79 ns |        23.703 ns |        22.172 ns |
-| EnqueueDequeue_PriorityQueue   | 100   |         7,806.20 ns |       105.801 ns |        98.967 ns |
-| Enqueue_RcSortedQueue          | 1000  |         7,566.23 ns |       149.010 ns |       218.418 ns |
-| Enqueue_RcBinaryMinHeap        | 1000  |        36,277.43 ns |        96.710 ns |        90.462 ns |
-| Enqueue_PriorityQueue          | 1000  |        28,564.19 ns |       186.866 ns |       174.795 ns |
-| DequeueAll_RcSortedQueue       | 1000  |       108,574.26 ns |       745.459 ns |       697.303 ns |
-| DequeueAll_RcBinaryMinHeap     | 1000  |       210,346.25 ns |       332.478 ns |       311.000 ns |
-| DequeueAll_PriorityQueue       | 1000  |       189,536.32 ns |     1,180.045 ns |     1,046.079 ns |
-| EnqueueDequeue_RcSortedQueue   | 1000  |     8,957,965.42 ns |    45,715.567 ns |    42,762.370 ns |
-| EnqueueDequeue_RcBinaryMinHeap | 1000  |       131,615.02 ns |       394.216 ns |       368.750 ns |
-| EnqueueDequeue_PriorityQueue   | 1000  |       114,799.89 ns |     1,269.621 ns |     1,060.191 ns |
-| Enqueue_RcSortedQueue          | 10000 |        77,143.76 ns |       996.372 ns |       932.007 ns |
-| Enqueue_RcBinaryMinHeap        | 10000 |       417,620.57 ns |       853.343 ns |       756.466 ns |
-| Enqueue_PriorityQueue          | 10000 |       278,791.68 ns |     1,566.093 ns |     1,464.924 ns |
-| DequeueAll_RcSortedQueue       | 10000 |     1,435,539.99 ns |     9,329.910 ns |     8,727.204 ns |
-| DequeueAll_RcBinaryMinHeap     | 10000 |     2,956,366.90 ns |     6,344.030 ns |     5,934.210 ns |
-| DequeueAll_PriorityQueue       | 10000 |     2,642,186.54 ns |     9,482.374 ns |     8,869.819 ns |
-| EnqueueDequeue_RcSortedQueue   | 10000 | 1,318,277,320.00 ns | 6,725,701.525 ns | 6,291,225.379 ns |
-| EnqueueDequeue_RcBinaryMinHeap | 10000 |     1,712,170.68 ns |     5,674.513 ns |     5,307.943 ns |
-| EnqueueDequeue_PriorityQueue   | 10000 |     1,466,910.77 ns |     4,394.686 ns |     4,110.792 ns |
+| Method                                    | Count | Mean             | Error           | StdDev          | Median           | Gen0    | Gen1    | Gen2    | Allocated |
+|------------------------------------------ |------ |-----------------:|----------------:|----------------:|-----------------:|--------:|--------:|--------:|----------:|
+| Enqueue_RcSortedQueue                     | 100   |         873.2 ns |        17.43 ns |        29.60 ns |         872.6 ns |  0.5589 |  0.0076 |       - |   4.57 KB |
+| Enqueue_RcBinaryMinHeap                   | 100   |       2,172.9 ns |        43.31 ns |       101.24 ns |       2,176.6 ns |  0.3929 |  0.0038 |       - |   3.21 KB |
+| Enqueue_PriorityQueue                     | 100   |       1,220.7 ns |        23.36 ns |        59.89 ns |       1,217.6 ns |  0.4883 |  0.0057 |       - |      4 KB |
+| EnqueueAll_DequeueAll_RcSortedQueue       | 100   |       5,493.5 ns |       504.63 ns |     1,487.91 ns |       4,680.8 ns |  0.5569 |       - |       - |   4.57 KB |
+| EnqueueAll_DequeueAll_RcBinaryMinHeap     | 100   |       6,625.9 ns |       122.18 ns |       120.00 ns |       6,636.7 ns |  0.3967 |       - |       - |   3.24 KB |
+| EnqueueAll_DequeueAll_PriorityQueue       | 100   |       6,329.1 ns |       121.57 ns |       124.84 ns |       6,344.6 ns |  0.4883 |       - |       - |      4 KB |
+| EnqueueDequeue_RcSortedQueue              | 100   |      38,617.2 ns |       765.68 ns |       678.75 ns |      38,637.2 ns |  0.4272 |       - |       - |   3.55 KB |
+| EnqueueDequeue_RcBinaryMinHeap            | 100   |       4,646.3 ns |        46.78 ns |        39.06 ns |       4,649.7 ns |  0.3967 |       - |       - |   3.24 KB |
+| EnqueueDequeue_PriorityQueue              | 100   |       3,891.3 ns |        77.75 ns |        95.48 ns |       3,886.7 ns |  0.4883 |       - |       - |      4 KB |
+| Enqueue_RcSortedQueue                     | 10000 |     247,050.4 ns |     4,529.12 ns |     3,782.02 ns |     246,733.4 ns | 83.0078 | 82.5195 | 41.5039 | 490.78 KB |
+| Enqueue_RcBinaryMinHeap                   | 10000 |     281,605.3 ns |     5,517.52 ns |     5,161.09 ns |     282,650.0 ns | 38.0859 | 24.9023 |       - | 312.59 KB |
+| Enqueue_PriorityQueue                     | 10000 |     279,167.6 ns |     5,559.77 ns |    11,848.32 ns |     282,628.6 ns | 49.8047 | 49.8047 | 49.8047 | 390.74 KB |
+| EnqueueAll_DequeueAll_RcSortedQueue       | 10000 |   1,145,484.0 ns |    13,429.18 ns |    12,561.66 ns |   1,145,563.3 ns | 82.0313 | 80.0781 | 41.0156 | 490.78 KB |
+| EnqueueAll_DequeueAll_RcBinaryMinHeap     | 10000 |   2,011,314.3 ns |    14,635.49 ns |    12,221.30 ns |   2,012,173.4 ns | 35.1563 | 11.7188 |       - | 312.62 KB |
+| EnqueueAll_DequeueAll_PriorityQueue       | 10000 |   2,049,687.6 ns |    13,863.42 ns |    12,967.85 ns |   2,052,782.4 ns | 46.8750 | 46.8750 | 46.8750 | 390.74 KB |
+| EnqueueDequeue_RcSortedQueue              | 10000 | 889,071,578.6 ns | 4,148,761.86 ns | 3,677,769.33 ns | 888,254,250.0 ns |       - |       - |       - | 363.17 KB |
+| EnqueueDequeue_RcBinaryMinHeap            | 10000 |   1,140,019.5 ns |    10,311.67 ns |     9,141.02 ns |   1,140,033.8 ns | 37.1094 | 17.5781 |       - | 312.62 KB |
+| EnqueueDequeue_PriorityQueue              | 10000 |   1,183,681.4 ns |     6,984.25 ns |     6,191.35 ns |   1,181,323.3 ns | 48.8281 | 48.8281 | 48.8281 | 390.74 KB |
 
 */
+
+public class TotalNode
+{
+    public int Id;
+    public float Total;
+}
 
 [MemoryDiagnoser]
 public class PriorityQueueBenchmarks
 {
-    [Params(10, 100, 1000, 10000)]
-    public int Count;
-
-    private RcSortedQueue<Node> _sq;
-    private RcBinaryMinHeap<Node> _bmHeap;
-    private PriorityQueue<Node, Node> _pq;
+    [Params(100, 10000)] public int Count;
 
     private float[] _priority;
 
-    class Node
+    private static int Comp(TotalNode x, TotalNode y)
     {
-        public int id;
-        public float total;
+        var v = x.Total.CompareTo(y.Total);
+        if (v != 0)
+            return v;
+        return x.Id.CompareTo(y.Id);
     }
+
 
     [GlobalSetup]
     public void Setup()
     {
-        static int Comp(Node x, Node y)
-        {
-            var v = x.total.CompareTo(y.total);
-            if (v != 0)
-                return v;
-            return x.id.CompareTo(y.id);
-        }
-
-        _sq = new(Comp);
-        _bmHeap = new(Count, Comp);
-        _pq = new(Count, Comparer<Node>.Create(Comp));
-
         _priority = new float[Count];
         for (int i = 0; i < Count; i++)
         {
@@ -98,13 +71,13 @@ public class PriorityQueueBenchmarks
     [Benchmark]
     public void Enqueue_RcSortedQueue()
     {
-        _sq.Clear();
+        RcSortedQueue<TotalNode> sq = new RcSortedQueue<TotalNode>(Comp);
         for (int i = 0; i < Count; i++)
         {
-            _sq.Enqueue(new Node
+            sq.Enqueue(new TotalNode
             {
-                id = i,
-                total = _priority[i],
+                Id = i,
+                Total = _priority[i],
             });
         }
     }
@@ -112,13 +85,13 @@ public class PriorityQueueBenchmarks
     [Benchmark]
     public void Enqueue_RcBinaryMinHeap()
     {
-        _bmHeap.Clear();
+        RcBinaryMinHeap<TotalNode> bmHeap = new RcBinaryMinHeap<TotalNode>(Count, Comp);
         for (int i = 0; i < Count; i++)
         {
-            _bmHeap.Push(new Node
+            bmHeap.Push(new TotalNode
             {
-                id = i,
-                total = _priority[i],
+                Id = i,
+                Total = _priority[i],
             });
         }
     }
@@ -126,73 +99,73 @@ public class PriorityQueueBenchmarks
     [Benchmark]
     public void Enqueue_PriorityQueue()
     {
-        _pq.Clear();
+        PriorityQueue<TotalNode, TotalNode> pg = new PriorityQueue<TotalNode, TotalNode>(Count, Comparer<TotalNode>.Create(Comp));
         for (int i = 0; i < Count; i++)
         {
-            var node = new Node
+            var node = new TotalNode
             {
-                id = i,
-                total = _priority[i],
+                Id = i,
+                Total = _priority[i],
             };
-            _pq.Enqueue(node, node);
+            pg.Enqueue(node, node);
         }
     }
 
     [Benchmark]
-    public void DequeueAll_RcSortedQueue()
+    public void EnqueueAll_DequeueAll_RcSortedQueue()
     {
-        _sq.Clear();
+        RcSortedQueue<TotalNode> sq = new RcSortedQueue<TotalNode>(Comp);
         for (int i = 0; i < Count; i++)
         {
-            _sq.Enqueue(new Node
+            sq.Enqueue(new TotalNode
             {
-                id = i,
-                total = _priority[i],
+                Id = i,
+                Total = _priority[i],
             });
         }
 
-        while (_sq.Count() > 0)
+        while (sq.Count() > 0)
         {
-            _sq.Dequeue();
+            sq.Dequeue();
         }
     }
 
     [Benchmark]
-    public void DequeueAll_RcBinaryMinHeap()
+    public void EnqueueAll_DequeueAll_RcBinaryMinHeap()
     {
-        _bmHeap.Clear();
+        RcBinaryMinHeap<TotalNode> bmHeap = new RcBinaryMinHeap<TotalNode>(Count, Comp);
         for (int i = 0; i < Count; i++)
         {
-            _bmHeap.Push(new Node
+            bmHeap.Push(new TotalNode
             {
-                id = i,
-                total = _priority[i],
+                Id = i,
+                Total = _priority[i],
             });
         }
 
-        while (_bmHeap.Count > 0)
+        while (bmHeap.Count > 0)
         {
-            _bmHeap.Pop();
+            bmHeap.Pop();
         }
     }
 
     [Benchmark]
-    public void DequeueAll_PriorityQueue()
+    public void EnqueueAll_DequeueAll_PriorityQueue()
     {
-        _pq.Clear();
+        PriorityQueue<TotalNode, TotalNode> pq = new PriorityQueue<TotalNode, TotalNode>(Count, Comparer<TotalNode>.Create(Comp));
         for (int i = 0; i < Count; i++)
         {
-            var node = new Node
+            var node = new TotalNode
             {
-                id = i,
-                total = _priority[i],
+                Id = i,
+                Total = _priority[i],
             };
-            _pq.Enqueue(node, node);
+            pq.Enqueue(node, node);
         }
 
-        while (_pq.Count > 0)
+        while (pq.Count > 0)
         {
-            _pq.Dequeue();
+            pq.Dequeue();
         }
     }
 
@@ -200,79 +173,79 @@ public class PriorityQueueBenchmarks
     [Benchmark]
     public void EnqueueDequeue_RcSortedQueue()
     {
-        _sq.Clear();
+        RcSortedQueue<TotalNode> sq = new RcSortedQueue<TotalNode>(Comp);
         int half = Count / 2;
         for (int i = 0; i < half; i++)
         {
-            _sq.Enqueue(new Node
+            sq.Enqueue(new TotalNode
             {
-                id = i,
-                total = _priority[i],
+                Id = i,
+                Total = _priority[i],
             });
         }
 
         for (int i = half; i < Count; i++)
         {
-            _sq.Enqueue(new Node
+            sq.Enqueue(new TotalNode
             {
-                id = i,
-                total = _priority[i],
+                Id = i,
+                Total = _priority[i],
             });
 
-            _sq.Dequeue();
+            sq.Dequeue();
         }
     }
 
     [Benchmark]
     public void EnqueueDequeue_RcBinaryMinHeap()
     {
-        _bmHeap.Clear();
+        RcBinaryMinHeap<TotalNode> bmHeap = new RcBinaryMinHeap<TotalNode>(Count, Comp);
         int half = Count / 2;
         for (int i = 0; i < half; i++)
         {
-            _bmHeap.Push(new Node
+            bmHeap.Push(new TotalNode
             {
-                id = i,
-                total = _priority[i],
+                Id = i,
+                Total = _priority[i],
             });
         }
 
         for (int i = half; i < Count; i++)
         {
-            _bmHeap.Push(new Node
+            bmHeap.Push(new TotalNode
             {
-                id = i,
-                total = _priority[i],
+                Id = i,
+                Total = _priority[i],
             });
 
-            _bmHeap.Pop();
+            bmHeap.Pop();
         }
     }
 
     [Benchmark]
     public void EnqueueDequeue_PriorityQueue()
     {
-        _pq.Clear();
+        PriorityQueue<TotalNode, TotalNode> pq = new PriorityQueue<TotalNode, TotalNode>(Count, Comparer<TotalNode>.Create(Comp));
         int half = Count / 2;
         for (int i = 0; i < half; i++)
         {
-            var node = new Node
+            var node = new TotalNode
             {
-                id = i,
-                total = _priority[i],
+                Id = i,
+                Total = _priority[i],
             };
-            _pq.Enqueue(node, node);
+            pq.Enqueue(node, node);
         }
 
         for (int i = half; i < Count; i++)
         {
-            var node = new Node
+            var node = new TotalNode
             {
-                id = i,
-                total = _priority[i],
+                Id = i,
+                Total = _priority[i],
             };
-            _pq.Enqueue(node, node);
-            _pq.Dequeue();
+            pq.Enqueue(node, node);
+            pq.Dequeue();
         }
     }
 }
